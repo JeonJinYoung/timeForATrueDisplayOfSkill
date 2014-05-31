a550fb14a0201d5e1f07c2d68ab9a2f50dbeaa3c
gnomgrol <a.baumunk@gmx.net>
2014-05-31 21:51:09 +0200

    asd

--- timeForATrueDisplayOfSkill.lua
+++ timeForATrueDisplayOfSkill.lua
@@ -8,7 +8,7 @@ require "VPrediction"
 
 local UPDATE_HOST      = "raw.github.com"
 local UPDATE_PATH      = "/gnomgrol/timeForATrueDisplayOfSkill/b417854906719289ed2a4d5f577cc2ffa9391ee8/timeForATrueDisplayOfSkill.lua"
-local VERSION_PATH     = 
+local VERSION_PATH     = "/gnomgrol/timeForATrueDisplayOfSkill/master/timeForATrueDisplayOfSkill.version"
 local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
 local UPDATE_URL       = "https://"..UPDATE_HOST..UPDATE_PATH
 local SCRIPT_NAME      = "Time for a true display of skill"
@@ -17,7 +17,7 @@ local version = 0.2
 local AUTOUPDATE = true
 
 if AUTOUPDATE then
-	 SourceUpdater(SCRIPT_NAME, version, UPDATE_HOST, UPDATE_PATH, UPDATE_FILE_PATH, "/honda7/BoL/master/VersionFiles/"..SCRIPT_NAME..".version"):CheckUpdate()
+	 SourceUpdater(SCRIPT_NAME, version, UPDATE_HOST, UPDATE_PATH, UPDATE_FILE_PATH, VERSION_PATH):CheckUpdate()
 end
 
 
